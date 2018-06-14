4e337049cb782b0f820a9f6d7add9e38c1fb1e8f
Billy Derringer <billyderringer7@gmail.com>
2018-06-14 04:11:55 -0500

    added remove & add functionality **remove not functioning properly

--- src/Note.js
+++ src/Note.js
@@ -7,7 +7,6 @@ class Note extends Component{
     constructor(props) {
         super(props);
         this.state={
-            text: '#MakeANote',
             editing:false
         }
 
@@ -15,14 +14,13 @@ class Note extends Component{
         this.renderDisplay = this.renderDisplay.bind(this)
         this.save = this.save.bind(this)
         this.edit = this.edit.bind(this)
-        this.complete = this.complete.bind(this)
         this.remove = this.remove.bind(this)
     }
 
-    save(){
-        let newText = this._newText.value === '' ? '#MakeANote' : this._newText.value
+    save(e){
+        e.preventDefault()
+        this.props.onChange(this._newText.value, this.props.index)
         this.setState({
-            text: newText,
             editing:false
         })
     }
@@ -33,29 +31,27 @@ class Note extends Component{
         })
     }
 
-    complete(){
-        alert('item completed')
-    }
-
     remove(){
-        alert('removing note')
+        this.props.onRemove(this.props.index)
     }
 
     renderForm(){
         return(
             <div className="note">
-                <textarea name="edit-form"
-                          id="edit-form"
-                          defaultValue={this.state.text}
-                          ref={input => this._newText = input}
-                          cols="1"
-                          rows="3" />
-                <span className="btn-row">
-                    <button onClick={this.save}
-                            id="save">
-                        <FaFloppyO />
-                    </button>
-                </span>
+                <form onSubmit={this.save}>
+                    <textarea name="edit-form"
+                              id="edit-form"
+                              defaultValue={this.props.children}
+                              ref={input => this._newText = input}
+                              cols="1"
+                              rows="3" />
+                    <span className="btn-row">
+                        <button onClick={this.save}
+                                id="save">
+                            <FaFloppyO />
+                        </button>
+                     </span>
+                </form>
             </div>
         )
     }
@@ -63,7 +59,7 @@ class Note extends Component{
     renderDisplay(){
         return(
             <div className="note drop-shadow lifted">
-                <p>{this.state.text}</p>
+                <p>{this.props.children}</p>
                 <span className="btn-row">
                     <button id="edit"
                             onClick={this.edit}>
